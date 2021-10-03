68e3f533b7b3656fbc4f5600ddcccb5d018e4c0b
Dhia Nabli <dhianabli@outlook.com>
2021-10-03 18:24:56 +0200

    improve input error handling

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import pandas
 
-# TODO 1. Create a dictionary in this format:
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 phonetic_code = {row.letter: row.code for (index, row) in data.iterrows()}
 
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-word = input("Enter Word: ").upper()
+def prompt():
+    word = input("Enter Word: ").upper()
+    try:
+        result = [phonetic_code[letter] for letter in word]
+    except KeyError:
+        print("sorry only letters in the alphabet please")
+        prompt()
+    else:
+        print(result)
 
-result = [phonetic_code[letter] for letter in word]
 
-print(result)
+prompt()
 
 
